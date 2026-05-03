9f5fb7e1fca59b68c236f90a333d137f86632ae8
leandroltfx <leandroltfbr@hotmail.com>
2026-05-03 20:46:11 -0300

    Inicia o projeto

---
+++ src/main/java/br/com/poc_1_kafka_leitura_pedido_ms_springboot/Poc1KafkaLeituraPedidoMsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.poc_1_kafka_leitura_pedido_ms_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poc1KafkaLeituraPedidoMsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poc1KafkaLeituraPedidoMsSpringbootApplication.class, args);
+	}
+
+}
