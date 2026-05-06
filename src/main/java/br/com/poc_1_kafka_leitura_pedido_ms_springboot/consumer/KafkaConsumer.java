2af3ed77b85a0ea8c7709c4014d800d3ed3a6113
leandroltfx <leandroltfbr@hotmail.com>
2026-05-06 19:51:34 -0300

    Renomeia classes

--- src/main/java/br/com/poc_1_kafka_leitura_pedido_ms_springboot/consumer/KafkaConsumer.java
+++ src/main/java/br/com/poc_1_kafka_leitura_pedido_ms_springboot/consumer/KafkaConsumer.java
@@ -1,6 +1,6 @@
 package br.com.poc_1_kafka_leitura_pedido_ms_springboot.consumer;
 
-import br.com.poc_1_kafka_leitura_pedido_ms_springboot.dto.PedidoCriadoDTO;
+import br.com.poc_1_kafka_leitura_pedido_ms_springboot.dto.ConsumerDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class PedidoCriadoConsumer {
+public class KafkaConsumer {
 
     private final ObjectMapper objectMapper;
 
@@ -20,7 +20,7 @@ public class PedidoCriadoConsumer {
     )
     public void listen(String json) {
         try {
-            var pedidoCriado = objectMapper.readValue(json, PedidoCriadoDTO.class);
+            var pedidoCriado = objectMapper.readValue(json, ConsumerDTO.class);
             log.info("Pedido recebido com sucesso", pedidoCriado);
         } catch (Exception e) {
             throw new RuntimeException(e);
---
+++ src/main/java/br/com/poc_1_kafka_leitura_pedido_ms_springboot/dto/ConsumerDTO.java
@@ -0,0 +1,6 @@
+package br.com.poc_1_kafka_leitura_pedido_ms_springboot.dto;
+
+public record ConsumerDTO(
+        String message
+) {
+}
