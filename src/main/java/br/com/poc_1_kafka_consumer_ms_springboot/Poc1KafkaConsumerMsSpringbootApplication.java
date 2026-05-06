6f044d170b50a3b8fbf76771bc9d48ac868a506c
leandroltfx <leandroltfbr@hotmail.com>
2026-05-06 20:29:59 -0300

    Renomeia classes e pacotes

---
+++ src/main/java/br/com/poc_1_kafka_consumer_ms_springboot/Poc1KafkaConsumerMsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.poc_1_kafka_consumer_ms_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poc1KafkaConsumerMsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poc1KafkaConsumerMsSpringbootApplication.class, args);
+	}
+
+}
--- src/test/java/br/com/poc_1_kafka_leitura_pedido_ms_springboot/Poc1KafkaConsumerMsSpringbootApplicationTests.java
+++ src/test/java/br/com/poc_1_kafka_leitura_pedido_ms_springboot/Poc1KafkaConsumerMsSpringbootApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Poc1KafkaLeituraPedidoMsSpringbootApplicationTests {
+class Poc1KafkaConsumerMsSpringbootApplicationTests {
 
 	@Test
 	void contextLoads() {
